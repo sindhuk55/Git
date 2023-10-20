d6d2a515e6a3c4aad04db70f3f6f641934357e0f
sindhuk55 <sindhusea555@gmail.com>
2023-10-20 18:45:59 +0530

    Second project

---
+++ org/omr/GitBash.java
@@ -0,0 +1,15 @@
+package org.omr;
+
+public class GitBash {
+	
+    public void Hello()
+    {
+    	System.out.println("Hello world");
+    }
+    public static void main(String args[])
+    {
+    	GitBash g=new GitBash();
+    	g.Hello();
+    	
+    }
+}
